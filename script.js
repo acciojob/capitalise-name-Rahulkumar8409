9082a998f2482a800eec1e54fe62f9d67c5392d5
Rahulkumar8409 <122075648+Rahulkumar8409@users.noreply.github.com>
2024-01-16 18:09:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+ function capitalizeName() {
+        var inputField = document.getElementById("fname");
+        inputField.value = inputField.value.toUpperCase();
+    }
